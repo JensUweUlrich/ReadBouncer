8fc892f6ca6a82893a111ee5fdaf68f77eb3574e
lutfia95 <62063481+lutfia95@users.noreply.github.com>
2020-05-19 15:25:37 +0200

    Add files via upload

---
+++ idea.cpp
@@ -0,0 +1,40 @@
+//Not run
+// defeniere eine globale Variable int active_threads = 0 ; 
+// Die Funktion Threading All 
+threading_all{
+    vec shapes = shape_generator();
+
+    for(shape s in shapes )
+    {
+        while (active_threads == 25)
+        
+            sleep(1000); // damit ist 1 Sekunbde gemeint , nachschauen  --> In Header hinzufügen , nachschauen 
+
+        
+        thread t = thread(run_program ,s)
+        active_threads++;
+        t.start();
+    }
+}
+// die Idee ist es , nach jedem Thread eine Puase zu haben von 1 Sekunde #+
+
+
+// run_program soll wie folgt verändert werden : 
+run_prpgram(shape s)
+{
+    CustomBloomFilter bf = CreatBloomFilter(s,...);
+    .....
+}
+CreatBloomFilter(s,...)
+{
+    ComputeMinimizer(s,.....);
+}
+// TODO :
+/*
+1) add sleep to header ThreadPool 
+2) Creat BloomFilter soll keine Ausgaben mehr haben 
+3) ComputeMinimizer auch 
+4) Defeniere Vector von Shapes als glbale Variable , dann die Shapes zu dem Hinzufügen dann verwenden 
+in Sketching und Minimizer 
+5) Addiere eine neue Parameter zu run_programm, ComputeMinimizer, Sketching
+*/ 
