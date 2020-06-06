7bcdc76e67c25ffc15d69ddd0844832e7377b5ae
lutfia95 <62063481+lutfia95@users.noreply.github.com>
2020-06-06 14:56:43 +0200

    Add files via upload

---
+++ ThreadPool.hpp
@@ -0,0 +1,98 @@
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <future>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <algorithm>
+
+
+
+#ifndef THREAD_POOL_HPP
+#define THREAD_POOL_HPP
+
+class ThreadPool
+{
+public:
+    using Task = std::function<void()>;
+
+    explicit ThreadPool(std::size_t numThreads)
+    {
+        start(numThreads);
+    }
+
+    ~ThreadPool()
+    {
+        stop();
+    }
+
+    template<class T>
+    auto threading(T task)->std::future<decltype(task())>
+    {
+        auto wrapper = std::make_shared<std::packaged_task<decltype(task()) ()>>(std::move(task));
+
+        {
+            std::unique_lock<std::mutex> lock{mEventMutex};
+            mTasks.emplace([=] {
+                (*wrapper)();
+            });
+        }
+
+        mEventVar.notify_one();
+        return wrapper->get_future();
+    }
+
+private:
+    std::vector<std::thread> mThreads;
+
+    std::condition_variable mEventVar;
+
+    std::mutex mEventMutex;
+    bool mStopping = false;
+
+    std::queue<Task> mTasks;
+
+    void start(size_t numThreads)
+    {
+        for (auto i = 0u; i < numThreads; ++i)
+        {
+            mThreads.emplace_back([=] {
+                while (true)
+                {
+                    Task task;
+
+                    {
+                        std::unique_lock<std::mutex> lock{mEventMutex};
+
+                        mEventVar.wait(lock, [=] { return mStopping || !mTasks.empty(); });
+
+                        if (mStopping && mTasks.empty())
+                            break;
+
+                        task = std::move(mTasks.front());
+                        mTasks.pop();
+                    }
+
+                    task();
+                }
+            });
+        }
+    }
+
+    void stop() noexcept
+    {
+        {
+            std::unique_lock<std::mutex> lock{mEventMutex};
+            mStopping = true;
+        }
+
+        mEventVar.notify_all();
+
+        for (auto &thread : mThreads)
+            thread.join();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+};
+
+#endif
