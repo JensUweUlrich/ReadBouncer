428de16d41f202c47813e48fc884dcda618df2da
lutfia95 <62063481+lutfia95@users.noreply.github.com>
2021-05-06 15:40:52 +0200

    added functional qt

--- src/main/ibfbuild.hpp
+++ src/main/ibfbuild.hpp
@@ -5,11 +5,15 @@
  *      Author: jens-uwe.ulrich
  */
 
+
 /**
 * initialize config forand build IBF
 * @parser	: input from the command line for "build" command
 * @throws : IBFBuildException
 */
+
+//ibf_build_parser ibfbuild_parser{cli};
+//buildIBF(ibfbuild_parser);
 void buildIBF(ibf_build_parser & parser)
 {
 	std::shared_ptr<spdlog::logger> nanolive_logger = spdlog::get("NanoLiveLog");
@@ -44,4 +48,44 @@ void buildIBF(ibf_build_parser & parser)
 		throw;
 	}
 
+}
+
+
+
+void buildIBF_qt (int k, int s, int f, int t, std::string ref_file_Name, std::string output_file_Name){
+    std::shared_ptr<spdlog::logger> nanolive_logger = spdlog::get("NanoLiveLog");
+    interleave::IBFConfig config{};
+
+    if (t < 1){t = 1;}
+    if (f < 10000){ f = 1000;}
+
+    config.reference_files.emplace_back(ref_file_Name);
+    config.output_filter_file =  output_file_Name;
+    config.kmer_size = k;
+    config.threads_build = t;
+    config.fragment_length = f;
+    config.filter_size = s;
+    //config.verbose = parser.verbose;
+
+    interleave::IBF filter{};
+    try
+    {
+        interleave::FilterStats stats = filter.create_filter(config);
+        interleave::print_stats(stats);
+    }
+    catch (const interleave::IBFBuildException& e)
+    {
+        nanolive_logger->error("Error building IBF using the following parameters");
+        nanolive_logger->error("Input reference file                : " +ref_file_Name);
+        nanolive_logger->error("Output IBF file                     : " + output_file_Name);
+        nanolive_logger->error("Kmer size                           : " + k);
+        nanolive_logger->error("Size of reference fragments per bin : " + f);
+        nanolive_logger->error("IBF file size in MegaBytes          : " + s);
+        nanolive_logger->error("Building threads                    : " + t);
+        nanolive_logger->error("Error message : " + std::string(e.what()));
+        nanolive_logger->error("---------------------------------------------------------------------------------------------------");
+        nanolive_logger->flush();
+        throw;
+    }
+    std::cout<< "the new f" << f <<std::endl;
 }
