31c959a804c728c87190638aa77eac1c6836960f
lutfia95 <62063481+lutfia95@users.noreply.github.com>
2022-11-28 12:57:40 +0100

    cmake linux

--- src/test/libIBFTests/IBFTest.cpp
+++ src/test/libIBFTests/IBFTest.cpp
@@ -12,12 +12,27 @@
 
 
 
+/*
+IBF.size() = 10
+add_seq_to_filter(IBF, "ACGT")
+FILTER_stat;
+std::string toClassify{ "ACGT" };
+toClassify.find(IBF);
+if (!notFound) {
 
+	gTest::Fail();
+}
+*/
+
+/*
+std::future<void> get(), asycn nachschauen 
+*/
 
 
 
 int main(int argc, char** argv)
 {
+	std::cout << "test1" << std::endl;
 	testing::InitGoogleTest(&argc, argv);
 	
 	return RUN_ALL_TESTS();
